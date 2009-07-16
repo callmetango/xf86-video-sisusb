35789a14c0074bb5994deb890e0f3832f1d4a067
Peter Hutterer <peter.hutterer@who-t.net>
2009-07-16 11:55:36 +1000

    Update to xextproto 7.1 support.
    DPMS header was split into dpms.h (client) and dpmsconst.h (server). Drivers
    need to include dpmsconst.h if xextproto 7.1 is available.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- src/sisusb_driver.c
+++ src/sisusb_driver.c
@@ -51,8 +51,13 @@
 
 #include "globals.h"
 
+#ifdef HAVE_XEXTPROTO_71
+#include <X11/extensions/dpmsconst.h>
+#else
 #define DPMS_SERVER
 #include <X11/extensions/dpms.h>
+#endif
+
 
 /*
  * This is intentionally screen-independent.  It indicates the binding
